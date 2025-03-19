b6faabf5a8d0d5a97e2cd4fd8704d6aaf124fbd2
criskwy <119081205+criskwy@users.noreply.github.com>
2025-03-19 16:48:20 -0600

    styling ticket

---
+++ main.js
@@ -0,0 +1,7 @@
+const nameAvatar = document.querySelector(".name-avatar");
+const emailAvatar = document.querySelector(".email-avatar");
+
+
+
+nameAvatar.textContent = "Jonatan Kristof";
+emailAvatar.textContent = "jonatan@email.com"
