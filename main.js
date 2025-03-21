6b78d4debe34f12066877d2c0f56245375092d35
criskwy <119081205+criskwy@users.noreply.github.com>
2025-03-21 16:24:55 -0600

    styling media queries views

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 const nameAvatar = document.querySelector(".name-avatar");
 const emailAvatar = document.querySelector(".email-avatar");
-
+const uploadCtn = document.querySelector(".upload-ctn");
 
 
 nameAvatar.textContent = "Jonatan Kristof";
 emailAvatar.textContent = "jonatan@email.com"
+
+
+
+uploadCtn.addEventListener("click", () => {
+  guardarImagen();
+});
+
