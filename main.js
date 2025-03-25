cabb6c6268f95ed7a7c48a1f5c5ff17187aa6c18
criskwy <119081205+criskwy@users.noreply.github.com>
2025-03-25 15:23:59 -0600

    new branch styling js

--- main.js
+++ main.js
@@ -1,14 +1,31 @@
 const nameAvatar = document.querySelector(".name-avatar");
 const emailAvatar = document.querySelector(".email-avatar");
 const uploadCtn = document.querySelector(".upload-ctn");
-
+const changeTicket = document.querySelector(".changeTicket")
+const container = document.querySelector(".container")
+const ticketCtn = document.querySelector(".ticket-ctn")
 
 nameAvatar.textContent = "Jonatan Kristof";
 emailAvatar.textContent = "jonatan@email.com"
 
+changeTicket.addEventListener("click", () => {
+  container.classList.toggle("change");
+  ticketCtn.classList.toggle("change");
+})
 
 
-uploadCtn.addEventListener("click", () => {
-  guardarImagen();
+uploadCtn.addEventListener("dragover", (e) => {
+  e.preventDefault();
+  uploadCtn.classList.add("active");
+});
+
+uploadCtn.addEventListener("dragleave", (e) => {
+  e.preventDefault();
+  uploadCtn.classList.remove("active");
+});
+
+uploadCtn.addEventListener("drop", (e) => {
+  e.preventDefault();
+  uploadCtn.classList.add("active");
 });
 
